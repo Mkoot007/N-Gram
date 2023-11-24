a939c605c9b715b743f63c070bce260b0a49b3d1
Moneeb Ahmad <89277472+Mkoot007@users.noreply.github.com>
2023-11-24 14:05:58 +0500

    Update N-gram.py

--- N-gram.py
+++ N-gram.py
@@ -1,13 +1,12 @@
 import streamlit as st
+import nltk
 from nltk import bigrams, word_tokenize
-
+nltk.download('punkt', download_dir='.')
 def extract_bigrams(text):
     words = word_tokenize(text)
     bigram_list = list(bigrams(words))
     return bigram_list
-
 st.title("Bigram Extractor")
-
 text_input = st.text_area("Enter text:", "Type or paste your text here.")
 
 if st.button("Extract Bigrams"):
